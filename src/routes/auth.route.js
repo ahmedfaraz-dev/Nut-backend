359dc34f566aa26877c95712cd564a0b59bb7e44
Ahmed_Faraz <ahmedfaraz031091@gmail.com>
2026-05-06 17:06:14 +0500

    implementing o auth

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,22 +1,17 @@
 import {Router} from "express";
 import { validateZodSchema } from "../middlewares/validateZodSchema.middleware.js";
 import { userLoginSchema } from "../schemas/userLogin.js";
-import { loginUser } from "../controllers/auth.controller.js";
+import { loginUser, googleAuthCallback, googleAuthFailed } from "../controllers/auth.controller.js";
+import passport from "../config/passport.js";
 
 const authRouter = Router();
 
 authRouter.route('/login').post(validateZodSchema(userLoginSchema), loginUser);
 
+authRouter.route('/google').get(passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+authRouter.route('/google/callback').get(passport.authenticate('google', {session: false, failureRedirect:`${process.env.CLIENT_URL}/api/v1/auth/google/failed` }), googleAuthCallback);
+  
 export {authRouter}
 
-//     url hit -->   m1  -->   m2-->  --> controller --> service=---> model/db
 
-//  server gracefullt shutdown 
-//   unhadnled promise  exception handle 
-
-// use helmet 
-// mongo sanitizer 
-// race condition  (find it) means do it all or do nothing
-// dual write issues  -> outbox pattern
-// rabbit mq
-//redis
